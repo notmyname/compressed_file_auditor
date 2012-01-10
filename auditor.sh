3daecc16b704087265710cd8d83bb0e2a105dea9
John Dickinson <me@not.mn>
2012-01-10 14:39:51 -0600

    made everything dynamic or settable

--- auditor.sh
+++ auditor.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 CHECKER=`which gunzip`
+CHECKER_OPTIONS="-t"
+MOVER=`which mv`
 QUAR=/tmp/
 
 while [ $1 ]; do
-    $CHECKER -t $1
+    $CHECKER $CHECKER_OPTIONS $1
     if [ $? != 0 ]; then
         # quarrantine
-        mv $1 $QUAR/$1
+        $MOVER $1 $QUAR/$1
     fi
     shift
 done
