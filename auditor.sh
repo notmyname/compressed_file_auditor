27135121df817590efa8a5da33347c47f3d0ef79
John Dickinson <me@not.mn>
2012-01-10 14:27:05 -0600

    initial import

---
+++ auditor.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+CHECKER=/usr/bin/gunzip
+QUAR=/tmp/
+
+while [ $1 ]; do
+    $CHECKER -t $1
+    if [ $? != 0 ]; then
+        # quarrantine
+        mv $1 $QUAR/$1
+    fi
+    shift
+done
